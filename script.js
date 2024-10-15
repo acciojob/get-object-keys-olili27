909d00fe9354a1d4ac32ca95ee9ad3c34e047335
Timothy Olili <timmolili27@gmail.com>
2024-10-15 23:55:06 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let obj = {
+  name: "student",
+  getKeys() {
+    let keys = Object.keys(obj);
+    return keys.filter((key) => key !== this.getKeys.name);
+  },
+};
+
+let student = Object.create(obj)
